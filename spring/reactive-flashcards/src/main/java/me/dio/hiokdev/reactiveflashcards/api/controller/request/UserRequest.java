e079c67a8fb34e6b9208ec5b1678a5bfa66af4db
Rodolfo HiOk <rodolfo.okino@gmail.com>
2024-05-01 00:00:05 -0300

    spring: fix docker config and add validation in user request

--- spring/reactive-flashcards/src/main/java/me/dio/hiokdev/reactiveflashcards/api/controller/request/UserRequest.java
+++ spring/reactive-flashcards/src/main/java/me/dio/hiokdev/reactiveflashcards/api/controller/request/UserRequest.java
@@ -1,11 +1,14 @@
 package me.dio.hiokdev.reactiveflashcards.api.controller.request;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.Builder;
 
 public record UserRequest(
-        @JsonProperty("name") String name,
-        @JsonProperty("email") String email
+        @JsonProperty("name") @NotBlank @Size(min = 3, max = 255) String name,
+        @JsonProperty("email") @NotBlank @Size(min = 3, max = 255) @Email String email
 ) {
 
     @Builder(toBuilder = true)
